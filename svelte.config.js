427d02badb209042b0f4c03099876ea82580cec0
Dmitry Novikov <kromez12@gmail.com>
2022-05-04 23:08:56 +0200

    push

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,22 @@
 import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify'
 
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		//target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-		}),
+
+        // preprocess: preprocess(),
+
+        adapter: adapter(),
+		// adapter: adapter({
+		// 	// default options are shown
+		// 	pages: 'build',
+		// 	assets: 'build',
+		// 	fallback: null,
+		// }),
 	},
 
 	preprocess: [
